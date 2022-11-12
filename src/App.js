977d0c34bae82d59013cc602a906f21382ae920d
rsmalberca <renzo.smalberca@gmail.com>
2022-11-12 17:47:47 -0500

    practicing with components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,36 @@ import ExpenseItem from "./Components/ExpenseItem";
 // Always make sure you are importing from the correct file, the correct folder, etc. Check, carefully, the names and the path.
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.72,
+      date: new Date(2020, 7, 4),
+    },
+    { id: "e2", title: "New TV", amount: 799.99, date: new Date(2021, 2, 12) },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.75,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450.65,
+      date: new Date(2021, 5, 12),
+    },
+  ];
   return (
     // HTML code inside a JS file??? WDF?!!!
     //well, this is JSX : JavaScript XML. This is allowed because all the transformations that happen behind the scenes.
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem name={expenses[0].title} qty={expenses[0].amount} expenseDate={expenses[0].date}></ExpenseItem>
+      <ExpenseItem name={expenses[1].title} qty={expenses[1].amount} expenseDate={expenses[1].date}></ExpenseItem>
+      <ExpenseItem name={expenses[2].title} qty={expenses[2].amount} expenseDate={expenses[2].date}></ExpenseItem>
+      <ExpenseItem name={expenses[3].title} qty={expenses[3].amount} expenseDate={expenses[3].date}></ExpenseItem>
     </div>
   );
 }
--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,11 +1,25 @@
-function ExpenseItem() {
+import "./ExpenseItem.css";
+//with this we bring all the CSS file into our JS file.
+
+function ExpenseItem(props) {
+  const month = props.expenseDate.toLocaleString('en-US', {month:'long'});
+  const day = props.expenseDate.toLocaleString('en-US', {day:'2-digit'});
+  const year = props.expenseDate.getFullYear();
+
   return (
     // It is important to have ONLY one root element for component, in this case the main div.
-    <div>
-      <div>July 3rd 2021</div>
+    //This is JSX code, this format (the type of code).
+
+    //So the key you access on your PROPS object has to be the name you picked for your attribute (prop) in the App.js file. In the <ExpenseItem> element.
+    <div className="expense-item ">
       <div>
-        <h2>Car Insurance</h2>
-        <div>$295.00</div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
+      <div className="expense-item__description">
+        <h2>{props.name}</h2>
+        <div className="expense-item__price">${props.qty}</div>
       </div>
     </div>
   );
